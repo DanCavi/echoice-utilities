8aed66178981309bdc25095b427def771bfd5973
Dante Cavieres <dcavieres@expertchoice.us.com>
2024-12-19 17:39:41 -0300

    Quitando cognito

--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -1,76 +1,80 @@
-import { lazy } from 'react';
+import { lazy } from "react";
 
 // project imports
-import MainLayout from 'layout/MainLayout';
-import Loadable from 'ui-component/Loadable';
-import RouteGuard from 'Aoth/RouteGuard';
-const Dashboard = Loadable(lazy(() => import('views/dashboard')));
-const DataSourceIntegration = Loadable(lazy(() => import('views/data-source-integration')));
-const WSGeneration = Loadable(lazy(() => import('views/ws-generation')));
-const WSUsers = Loadable(lazy(() => import('views/ws-users')));
-const SearchRut = Loadable(lazy(() => import('views/search')));
-const CrearEmpresa = Loadable(lazy(() => import('views/crear-empresa')));
-const Perfiles = Loadable(lazy(() => import('views/perfiles')));
-const Consumo = Loadable(lazy(() => import('views/consumo')));
-const SalidaWS = Loadable(lazy(() => import('views/salida-ws')));
-const VariableWorkshop = Loadable(lazy(() => import('views/variable-workshop')));
-const SystemOption = Loadable(lazy(() => import('views/system-option')));
+import MainLayout from "layout/MainLayout";
+import Loadable from "ui-component/Loadable";
+import RouteGuard from "Aoth/RouteGuard";
+const Dashboard = Loadable(lazy(() => import("views/dashboard")));
+const DataSourceIntegration = Loadable(
+  lazy(() => import("views/data-source-integration"))
+);
+const WSGeneration = Loadable(lazy(() => import("views/ws-generation")));
+const WSUsers = Loadable(lazy(() => import("views/ws-users")));
+const SearchRut = Loadable(lazy(() => import("views/search")));
+const CrearEmpresa = Loadable(lazy(() => import("views/crear-empresa")));
+const Perfiles = Loadable(lazy(() => import("views/perfiles")));
+const Consumo = Loadable(lazy(() => import("views/consumo")));
+const SalidaWS = Loadable(lazy(() => import("views/salida-ws")));
+const VariableWorkshop = Loadable(
+  lazy(() => import("views/variable-workshop"))
+);
+const SystemOption = Loadable(lazy(() => import("views/system-option")));
 
 // ==============================|| MAIN ROUTING ||============================== //
 
 const MainRoutes = {
-  path: '/',
-  element:
-    <RouteGuard>
-      <MainLayout />
-    </RouteGuard>
-    ,
+  path: "/",
+  element: (
+    // <RouteGuard>
+    <MainLayout />
+  ),
+  // {/* </RouteGuard> */}
   children: [
     {
-      path: '/',
-      element: <Dashboard />
+      path: "/",
+      element: <Dashboard />,
     },
     {
-      path: 'dashboard',
-      element: <Dashboard />
+      path: "dashboard",
+      element: <Dashboard />,
     },
     {
-      path: 'crear-empresa',
-      element: <CrearEmpresa />
+      path: "crear-empresa",
+      element: <CrearEmpresa />,
     },
     {
-      path: 'data-source-integration',
-      element: <DataSourceIntegration />
+      path: "data-source-integration",
+      element: <DataSourceIntegration />,
     },
     {
-      path: 'ws-users',
-      element: <WSUsers />
+      path: "ws-users",
+      element: <WSUsers />,
     },
     {
-      path: 'ws-generation',
-      element: <WSGeneration />
+      path: "ws-generation",
+      element: <WSGeneration />,
     },
     {
-      path: 'test',
-      element: <SearchRut />
+      path: "test",
+      element: <SearchRut />,
     },
     {
-      path: 'perfiles',
-      element: <Perfiles />
+      path: "perfiles",
+      element: <Perfiles />,
     },
     {
-      path: 'consumo',
-      element: <Consumo />
+      path: "consumo",
+      element: <Consumo />,
     },
     {
-      path: 'salida-ws',
-      element: <SalidaWS />
+      path: "salida-ws",
+      element: <SalidaWS />,
     },
     {
-      path: 'system-option',
-      element: <SystemOption />
-    }
-  ]
+      path: "system-option",
+      element: <SystemOption />,
+    },
+  ],
 };
 
 export default MainRoutes;
